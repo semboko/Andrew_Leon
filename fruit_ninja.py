87bfb297a772ec1245ddb08f4f1ec01b9197e2b0
Simon Bondarenko <bondarenko@vt.edu>
2024-04-02 16:17:53 -0600

    Added halves

--- fruit_ninja.py
+++ fruit_ninja.py
@@ -1,5 +1,5 @@
 import pygame
-from random import randint
+from random import randint, choice
 
 
 pygame.init()
@@ -11,18 +11,34 @@ w, h = bg_img.get_size()
 scale_factor = 0.7
 bg_img = pygame.transform.scale(bg_img, (w * scale_factor, h * scale_factor))
 
+sword_sounds = [
+    pygame.mixer.Sound("./assets/sword-1a.wav"),
+    pygame.mixer.Sound("./assets/sword-1b.wav"),
+    pygame.mixer.Sound("./assets/sword-arm-2a.wav"),
+    pygame.mixer.Sound("./assets/sword-arm-2b.wav"),
+]
+
 
 electolize_large = pygame.font.Font("./assets/Electrolize-Regular.ttf", 40)
 score = 0
 
 fruit_imgs = []
 
+half_imgs1 = []
+half_imgs2 = []
+
 for fruit_name in ("apple", "banana", "peach", "strawberry", "watermelon"):
     file_name = "./assets/" + fruit_name + ".png"
     img = pygame.image.load(file_name)
     fruit_imgs.append(img)
 
+    half_name1 = "./assets/" + fruit_name + "-1.png"
+    half_name2 = "./assets/" + fruit_name + "-2.png"
+    half_imgs1.append(pygame.image.load(half_name1))
+    half_imgs2.append(pygame.image.load(half_name2))
+
 fruits = []
+halves = []
 
 
 def spawn_fruit():
@@ -49,6 +65,9 @@ while True:
                 if fruit_hitbox.collidepoint(event.pos):
                     fruits.remove(fruit)
                     score += 2
+                    choice(sword_sounds).play()
+                    halves.append([x, y, half_imgs1[kind], -3, 0])
+                    halves.append([x, y, half_imgs2[kind], 3, 0])
 
     display.blit(bg_img, (-340, 0))
     for f in fruits:
@@ -62,6 +81,15 @@ while True:
 
     if randint(0, 45) == 0:
         spawn_fruit()
+
+    for half in halves:
+        x, y, img, xvel, yvel = half
+        display.blit(img, (x, y))
+        half[0] = half[0] + xvel
+        half[3] = half[3] * 0.9
+
+        half[1] = half[1] + yvel
+        half[4] = half[4] + 0.028
 
     pygame.display.update()
     clock.tick(60)
